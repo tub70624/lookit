a4c3fcb1078267be6e500e69b4fc3eda2de80342
Hakim Azly <hakim.azly@gmail.com>
2016-02-21 14:41:39 -0500

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lookit::Application.config.secret_key_base = '1618236224ef59f636bce90558e695fe4a8ceb2fe0a622ea6be7fbdfac654d711c616c9f38d7ed59606b2a0f8503654b5e2268f96b3a54856c5df7e24a844044'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lookit::Application.config.session_store :cookie_store, key: '_lookit_session'
